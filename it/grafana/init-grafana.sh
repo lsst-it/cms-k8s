8d282c400735c337f2d56fa52d6c8950a9233d0a
Heinrich Reinking <hreinkinge@gmail.com>
2020-10-05 14:15:51 -0300

    Initialisation script and dashboards for grafana

---
+++ it/grafana/init-grafana.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -xuo pipefail
+
+NAMESPACE='it-grafana'
+URL="$(kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]')"
+USER="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-user"]' | base64 --decode)"
+PASSWORD="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-password"]' | base64 --decode)"
+
+/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @'resources/CCS-Traffic.json'
+/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @'resources/CCS-Database.json'
