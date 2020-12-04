0568767a97c7d4683f02c773f258249316a0c356
Heinrich Reinking <hreinkinge@gmail.com>
2020-12-04 13:35:04 -0300

    add telegraf resources

--- it/influxdb/init-influxdb.sh
+++ it/influxdb/init-influxdb.sh
@@ -2,15 +2,15 @@
 set -xuo pipefail
 
 INFLUXDB_NAMESPACE='it-influxdb'
-GRAFANA_NAMESPACE='it-monitoring'
-DATABASE='grafana'
+INFLUXDB_USER='telegraf'
+GRAFANA_NAMESPACE='it-grafana'
+DATABASE='telegraf'
 
 INFLUXDB_HOST="$(kubectl -n ${INFLUXDB_NAMESPACE} get ingress influxdb -ojson | jq -r '.spec.tls[0].hosts[0]')"
-INFLUXDB_USER="$(kubectl -n ${INFLUXDB_NAMESPACE} get secret influxdb-credentials -ojson | jq -r '.data["influxdb-user"]' | base64 --decode)"
 INFLUXDB_PASSWORD="$(kubectl -n ${INFLUXDB_NAMESPACE} get secret influxdb-credentials -ojson | jq -r '.data["influxdb-password"]' | base64 --decode)"
 GRAFANA_PASSWORD="$(kubectl -n ${GRAFANA_NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-password"]' | base64 --decode)"
 
 kubectl exec -it -n ${INFLUXDB_NAMESPACE} influxdb-0 -- \
     /usr/bin/influx -host "${INFLUXDB_HOST}" -port 443 -ssl -username "${INFLUXDB_USER}" -password  "${INFLUXDB_PASSWORD}" -execute "CREATE DATABASE ${DATABASE}"
 kubectl exec -it -n ${INFLUXDB_NAMESPACE} influxdb-0 -- \
-    /usr/bin/influx -host "${INFLUXDB_HOST}" -port 443 -ssl -username "${INFLUXDB_USER}" -password  "${INFLUXDB_PASSWORD}" -execute "\"CREATE USER ${INFLUXDB_USER} WITH PASSWORD ${GRAFANA_PASSWORD} WITH ALL PRIVILEGES\""
+    /usr/bin/influx -host "${INFLUXDB_HOST}" -port 443 -ssl -username "${INFLUXDB_USER}" -password  "${INFLUXDB_PASSWORD}" -execute "USE DATABASE ${DATABASE;} CREATE USER telegraf WITH PASSWORD ${GRAFANA_PASSWORD} WITH ALL PRIVILEGES"
