c56a2c8304748b8fac3cd1763dd07538f4baf204
Heinrich Reinking <hreinkinge@gmail.com>
2020-12-09 13:11:48 -0300

    add BDC chart

--- it/grafana/init-grafana.sh
+++ it/grafana/init-grafana.sh
@@ -8,5 +8,4 @@ PASSWORD="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -
 POD="$(kubectl get -n ${NAMESPACE} pods -o json | jq -r '.items[].metadata.name')"
 
 /usr/bin/kubectl wait --for=condition=ready pod -n ${NAMESPACE} ${POD} > /dev/null 2>&1
-/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @'resources/CCS-Traffic.json' > /dev/null 2>&1
-/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @'resources/CCS-Database.json' > /dev/null 2>&1
+/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @'resources/BDC-Bandwidth.json' > /dev/null 2>&1
