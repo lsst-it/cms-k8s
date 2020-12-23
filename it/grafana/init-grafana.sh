e3b888c6391b91b186af856be9a2253487e6840c
Heinrich Reinking <hreinkinge@gmail.com>
2020-12-23 12:21:24 -0300

    add autodeploy to new dashboard

--- it/grafana/init-grafana.sh
+++ it/grafana/init-grafana.sh
@@ -9,3 +9,4 @@ POD="$(kubectl get -n ${NAMESPACE} pods -o json | jq -r '.items[].metadata.name'
 
 /usr/bin/kubectl wait --for=condition=ready pod -n ${NAMESPACE} ${POD} > /dev/null 2>&1
 /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @'resources/BDC-Bandwidth.json' > /dev/null 2>&1
+/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @'resources/CR-SummitTemp.json' > /dev/null 2>&1
