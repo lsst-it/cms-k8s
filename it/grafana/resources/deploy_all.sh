e592852ce5ffcb85a849644e67271587221fa144
Heinrich Reinking <hreinkinge@gmail.com>
2021-07-29 14:22:23 -0400

    update scripts and dashboards

--- it/grafana/resources/deploy_all.sh
+++ it/grafana/resources/deploy_all.sh
@@ -1,28 +1,76 @@
 #!/usr/bin/env bash
 set -xuo pipefail
 
-NAMESPACE='it-grafana'
-URL="$(kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]')"
-USER="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-user"]' | base64 --decode)"
-PASSWORD="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-password"]' | base64 --decode)"
-POD="$(kubectl get -n ${NAMESPACE} pods -o json | jq -r '.items[].metadata.name')"
-Folders=(servers clusters services)
-counter=3
-
-/usr/bin/kubectl wait --for=condition=ready pod -n ${NAMESPACE} ${POD} > /dev/null 2>&1
+URL="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
+USER="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
+PASSWORD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
+POD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
+Folders=(clusters servers services)
+counter=0
+# Folders Creation
+if [ ! -d "ID" ] 
+then
+    mkdir ID
+fi
+ssh hreinking_b@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 for folder in "${Folders[@]}"; do
-    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
-    counter=$((counter+1))
+  /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
+  /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X GET https://${URL}/api/folders | jq ".[$counter].id" > ID/$folder
+  counter=$((counter+1))
 done
 
-for filename in clusters/*.json; do
+#Clusters
+cd clusters
+if [ ! -d "list" ] 
+then
+    mkdir list
+fi
+cd default
+for filename in *.json; do
+    sed "s/\"folderId\": 4/\"folderId\": $ID/g" $filename > ../list/$filename
+done
+
+ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+
+cd ../list
+for filename in *.json; do
     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
 done
 
-for filename in servers/*.json; do
+#Services
+cd ../../services
+
+if [ ! -d "list" ] 
+then
+    mkdir list
+fi
+cd default
+for filename in *.json; do
+    sed "s/\"folderId\": 5/\"folderId\": $ID/g" $filename > ../list/$filename
+done
+
+ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+
+cd ../list
+for filename in *.json; do
     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
 done
 
-for filename in services/*.json; do
+#Services
+cd ../../servers
+
+if [ ! -d "list" ] 
+then
+    mkdir list
+fi
+while read server;
+do 
+  sed "s/SERVER_NAME/$server/g" server_template.json > default/$server.json
+  sed "s/\"folderId\": 3/\"folderId\": $ID/g" default/$server.json > list/$server.json
+done < server_list.txt
+
+
+ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+for filename in list/*.json; do
     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
 done
--- it/grafana/resources/folders_generator.sh
+++ it/grafana/resources/folders_generator.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -xuo pipefail
 
-NAMESPACE='it-grafana'
-URL="$(kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]')"
-USER="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-user"]' | base64 --decode)"
-PASSWORD="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-password"]' | base64 --decode)"
-POD="$(kubectl get -n ${NAMESPACE} pods -o json | jq -r '.items[].metadata.name')"
-Folders=(servers clusters services)
-counter=3
-
-/usr/bin/kubectl wait --for=condition=ready pod -n ${NAMESPACE} ${POD} > /dev/null 2>&1
+URL="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
+USER="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
+PASSWORD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
+POD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
+Folders=(clusters servers services)
+counter=0
+if [ ! -d "ID" ] 
+then
+    mkdir ID
+fi
+ssh hreinking_b@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 for folder in "${Folders[@]}"; do
-    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
-    counter=$((counter+1))
+  /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
+  /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X GET https://${URL}/api/folders | jq ".[$counter].id" > ID/$folder
+  counter=$((counter+1))
 done
