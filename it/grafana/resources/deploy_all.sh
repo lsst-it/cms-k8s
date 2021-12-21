a7a2907c5e283766bcf94dac2507f7aa3f80cd92
Heinrich Reinking <hreinkinge@gmail.com>
2021-12-21 14:44:18 -0300

    minor fixes

--- it/grafana/resources/deploy_all.sh
+++ it/grafana/resources/deploy_all.sh
@@ -6,13 +6,14 @@ USER="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get s
 PASSWORD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
 POD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
 Folders=(clusters servers services)
+SSH_USER="hreinking_b"
 counter=0
 # Folders Creation
 if [ ! -d "ID" ] 
 then
     mkdir ID
 fi
-ssh hreinking_b@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 for folder in "${Folders[@]}"; do
   /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
   /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X GET https://${URL}/api/folders | jq ".[$counter].id" > ID/$folder
