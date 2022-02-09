4c5474a996a0c7e95eb186c55150fd3db4ba89e5
Heinrich Reinking <hreinkinge@gmail.com>
2022-02-09 13:04:54 -0300

    small fixes for new cluster

--- it/grafana/resources/deploy_all.sh
+++ it/grafana/resources/deploy_all.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -xuo pipefail
 
-URL="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
-USER="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
-PASSWORD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
-POD="$(ssh hreinking_b@ruka01.ls.lsst.org '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
+SERVER='luan01.ls.lsst.org'
+URL="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
+USER="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
+PASSWORD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
+POD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
 Folders=(clusters servers services)
 SSH_USER="hreinking_b"
 counter=0
@@ -13,7 +14,7 @@ if [ ! -d "ID" ]
 then
     mkdir ID
 fi
-ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 for folder in "${Folders[@]}"; do
   /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
   /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X GET https://${URL}/api/folders | jq ".[$counter].id" > ID/$folder
@@ -31,7 +32,7 @@ for filename in *.json; do
     sed "s/\"folderId\": 4/\"folderId\": $ID/g" $filename > ../list/$filename
 done
 
-ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 
 cd ../list
 for filename in *.json; do
@@ -50,7 +51,7 @@ for filename in *.json; do
     sed "s/\"folderId\": 5/\"folderId\": $ID/g" $filename > ../list/$filename
 done
 
-ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 
 cd ../list
 for filename in *.json; do
@@ -71,7 +72,7 @@ do
 done < server_list.txt
 
 
-ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 for filename in list/*.json; do
     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
 done
--- it/grafana/resources/services/services.sh
+++ it/grafana/resources/services/services.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -xuo pipefail
 
+SERVER='luan01.ls.lsst.org'
 ID=$(cat ../ID/services)
 SSH_USER='hreinking_b'
-URL="$(ssh $SSH_USER@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
-USER="$(ssh $SSH_USER@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
-PASSWORD="$(ssh $SSH_USER@ruka01.ls.lsst.org '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
-POD="$(ssh $SSH_USER@ruka01.ls.lsst.org '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
+URL="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
+USER="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
+PASSWORD="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
+POD="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
 
 if [ ! -d "list" ] 
 then
@@ -17,7 +18,7 @@ for filename in *.json; do
     sed "s/\"folderId\": 5/\"folderId\": $ID/g" $filename > ../list/$filename
 done
 
-ssh $SSH_USER@ruka01.ls.lsst.org "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 
 cd ../list
 for filename in *.json; do
