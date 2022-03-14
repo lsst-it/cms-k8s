2c726dd2d7febbc8df0911d425050843eb1510f0
Heinrich Reinking <hreinkinge@gmail.com>
2022-03-14 16:37:25 -0300

    fix deploy all script

--- it/dev/grafana/resources/deploy_all.sh
+++ it/dev/grafana/resources/deploy_all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -xuo pipefail
 
-SERVER='luan01.ls.lsst.org'
+SERVER='ruka01.dev.lsst.org'
 ROOT_PATH=$(pwd)
 URL="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
 USER="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
 PASSWORD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
 POD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
-Folders=(clusters servers services)
+Folders=(clusters servers services k8s-ruka)
 SSH_USER="hreinking_b"
 counter=0
 # Folders Creation
@@ -15,7 +15,9 @@ if [ ! -d "ID" ]
 then
     mkdir ID
 fi
+
 ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+
 for folder in "${Folders[@]}"; do
   /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
   /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X GET https://${URL}/api/folders | jq ".[$counter].id" > ID/$folder
@@ -34,8 +36,6 @@ for filename in *.json; do
     sed "s/\"folderId\": 4/\"folderId\": $CLUSTER_ID/g" $filename > ../list/$filename
 done
 
-ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
-
 cd ../list
 for filename in *.json; do
     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
@@ -52,8 +52,6 @@ cd default
 for filename in *.json; do
     sed "s/\"folderId\": 5/\"folderId\": $SERVICES_ID/g" $filename > ../list/$filename
 done
-
-ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 
 cd ../list
 for filename in *.json; do
@@ -77,14 +75,22 @@ do
   sed "s/\"folderId\": 3/\"folderId\": $SERVERS_ID/g" default/$server.json > list/$server.json
 done < server_list.txt
 
-
-ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
 for filename in list/*.json; do
     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
 done
 
-#Generic
-cd ../../generic
+#Ruka K8s metrics
+cd ../../k8s-ruka
+if [ ! -d "list" ] 
+then
+    mkdir list
+fi
+cd default
+for filename in *.json; do
+    sed "s/\"folderId\": 6/\"folderId\": $ID/g" $filename > ../list/$filename
+done
+
+cd ../list
 for filename in *.json; do
     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
 done
