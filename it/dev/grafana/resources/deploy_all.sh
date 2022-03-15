860d095f034b0e24c21af0590a1ebb419638b59e
Heinrich Reinking <hreinkinge@gmail.com>
2022-03-15 11:40:49 -0300

    fix ID at deploy_all

--- it/dev/grafana/resources/deploy_all.sh
+++ it/dev/grafana/resources/deploy_all.sh
@@ -9,7 +9,7 @@ PASSWORD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get secret g
 POD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
 Folders=(clusters servers services k8s-ruka)
 SSH_USER="hreinking_b"
-counter=0
+counter=$RANDOM
 # Folders Creation
 if [ ! -d "ID" ] 
 then
@@ -20,7 +20,7 @@ ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-gra
 
 for folder in "${Folders[@]}"; do
   /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
-  /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X GET https://${URL}/api/folders | jq ".[$counter].id" > ID/$folder
+  /usr/bin/curl -s -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X GET https://${URL}/api/folders | jq -r '.[] | "\(.title) \(.id)"' | grep $folder | awk '{print $2}' > ID/$folder
   counter=$((counter+1))
 done
 
@@ -80,18 +80,18 @@ done
 # done
 
 # #Ruka K8s metrics
-K8S_RUKA_ID=$(cat $ROOT_PATH/ID/k8s-ruka)
-cd k8s-ruka
-if [ ! -d "list" ] 
-then
-    mkdir list
-fi
-cd default
-for filename in *.json; do
-    sed "s/\"folderId\": 6/\"folderId\": $K8S_RUKA_ID/g" $filename > ../list/$filename
-done
+# K8S_RUKA_ID=$(cat $ROOT_PATH/ID/k8s-ruka)
+# cd k8s-ruka
+# if [ ! -d "list" ] 
+# then
+#     mkdir list
+# fi
+# cd default
+# for filename in *.json; do
+#     sed "s/\"folderId\": 6/\"folderId\": $K8S_RUKA_ID/g" $filename > ../list/$filename
+# done
 
-cd ../list
-for filename in *.json; do
-    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
-done
+# cd ../list
+# for filename in *.json; do
+#     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
+# done
