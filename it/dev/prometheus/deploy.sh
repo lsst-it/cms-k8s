d1746250db4817e5305030a5311142d2b5a9505f
Heinrich Reinking <hreinkinge@gmail.com>
2022-03-17 15:06:55 -0300

    update for exporting metrics

--- it/dev/prometheus/deploy.sh
+++ it/dev/prometheus/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xe
 
-kubectl create ns prometheus
+kubectl create ns ruka-prom
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus -n prometheus prometheus-community/kube-prometheus-stack -f values.yaml
+helm install ruka-prom -n ruka-prom prometheus-community/kube-prometheus-stack -f values.yaml
