e0c6feeab4f005d143e4ebb72337a7c27766ca82
Heinrich Reinking <hreinkinge@gmail.com>
2022-03-17 23:29:06 -0300

    add yagan k8s-cluster monitoring

--- it/cp/grafana/resources/deploy_all.sh
+++ it/cp/grafana/resources/deploy_all.sh
@@ -7,7 +7,7 @@ URL="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get ingress grafa
 USER="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
 PASSWORD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
 POD="$(ssh hreinking_b@$SERVER '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
-Folders=(clusters servers services k8s-yepun)
+Folders=(clusters servers services k8s-yepun k8s-yagan)
 SSH_USER="hreinking_b"
 counter=$RANDOM
 NUMBER='0'
@@ -30,61 +30,62 @@ declare CLUSTERS_ID="$(cat ID/clusters)"
 declare SERVICES_ID="$(cat ID/services)"
 declare SERVERS_ID="$(cat ID/servers)"
 declare K8S_YEPUN_ID="$(cat ID/k8s-yepun)"
+declare K8S_YAGAN_ID="$(cat ID/k8s-yagan)"
 
-#Clusters
-cd clusters
-if [ ! -d "list" ] 
-then
-    mkdir list
-fi
-cd default
-for filename in *.json; do
-    sed "s/\"folderId\": 4/\"folderId\": $CLUSTERS_ID/g" $filename > ../list/$filename
-done
+# #Clusters
+# cd clusters
+# if [ ! -d "list" ] 
+# then
+#     mkdir list
+# fi
+# cd default
+# for filename in *.json; do
+#     sed "s/\"folderId\": 4/\"folderId\": $CLUSTERS_ID/g" $filename > ../list/$filename
+# done
 
-cd ../list
-for filename in *.json; do
-    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
-done
+# cd ../list
+# for filename in *.json; do
+#     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
+# done
 
-#Services
-cd ../../services
-if [ ! -d "list" ] 
-then
-    mkdir list
-fi
-cd default
-for filename in *.json; do
-    sed "s/\"folderId\": 5/\"folderId\": $SERVICES_ID/g" $filename > ../list/$filename
-done
+# #Services
+# cd ../../services
+# if [ ! -d "list" ] 
+# then
+#     mkdir list
+# fi
+# cd default
+# for filename in *.json; do
+#     sed "s/\"folderId\": 5/\"folderId\": $SERVICES_ID/g" $filename > ../list/$filename
+# done
 
-cd ../list
-for filename in *.json; do
-    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
-done
+# cd ../list
+# for filename in *.json; do
+#     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
+# done
 
-#Servers
-cd ../../servers
-ssh -l $SSH_USER foreman.ls.lsst.org 'sudo /usr/bin/hammer host list --fields Name | grep lsst.org' > server_list.txt
-ssh -l $SSH_USER foreman.dev.lsst.org 'sudo /usr/bin/hammer host list --fields Name | grep lsst.org' >> server_list.txt
-ssh -l $SSH_USER foreman.tuc.lsst.cloud 'sudo /usr/bin/hammer host list --fields Name | grep lsst.org' >> server_list.txt
+# #Servers
+# cd ../../servers
+# ssh -l $SSH_USER foreman.ls.lsst.org 'sudo /usr/bin/hammer host list --fields Name | grep lsst.org' > server_list.txt
+# ssh -l $SSH_USER foreman.dev.lsst.org 'sudo /usr/bin/hammer host list --fields Name | grep lsst.org' >> server_list.txt
+# ssh -l $SSH_USER foreman.tuc.lsst.cloud 'sudo /usr/bin/hammer host list --fields Name | grep lsst.org' >> server_list.txt
 
-if [ ! -d "list" ] 
-then
-    mkdir list
-fi
-while read server;
-do 
-  sed "s/SERVER_NAME/$server/g" server_template.json > default/$server.json
-  sed "s/\"folderId\": 3/\"folderId\": $SERVERS_ID/g" default/$server.json > list/$server.json
-done < server_list.txt
+# if [ ! -d "list" ] 
+# then
+#     mkdir list
+# fi
+# while read server;
+# do 
+#   sed "s/SERVER_NAME/$server/g" server_template.json > default/$server.json
+#   sed "s/\"folderId\": 3/\"folderId\": $SERVERS_ID/g" default/$server.json > list/$server.json
+# done < server_list.txt
 
-for filename in list/*.json; do
-    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
-done
+# for filename in list/*.json; do
+#     /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
+# done
 
 #Yepun K8s metrics
-cd ../../k8s-yepun
+cd k8s-yepun
 if [ ! -d "list" ] 
 then
     mkdir list
@@ -92,6 +93,22 @@ fi
 cd default
 for filename in *.json; do
     sed "s/\"folderId\": 6/\"folderId\": $K8S_YEPUN_ID/g" $filename > ../list/$filename
+done
+
+cd ../list
+for filename in *.json; do
+    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
+done
+
+#Yagan K8s metrics
+cd ../../k8s-yagan
+if [ ! -d "list" ] 
+then
+    mkdir list
+fi
+cd default
+for filename in *.json; do
+    sed "s/\"folderId\": 6/\"folderId\": $K8S_YAGAN_ID/g" $filename > ../list/$filename
 done
 
 cd ../list
---
+++ it/cp/grafana/resources/k8s-yagan/k8s-yepun.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -xuo pipefail
+
+ID=$(cat ../ID/k8s-yagan)
+SERVER='yepun01.dev.lsst.org'
+SSH_USER='hreinking_b'
+URL="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].hosts[0]'')"
+USER="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-user"]' | base64 --decode)"
+PASSWORD="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl -n it-grafana get secret grafana-credentials -ojson ' | jq -r '.data["admin-password"]' | base64 --decode)"
+POD="$(ssh $SSH_USER@$SERVER '/usr/bin/kubectl get -n it-grafana pods -o json ' | jq -r '.items[].metadata.name')"
+
+if [ ! -d "list" ] 
+then
+    mkdir list
+fi
+cd default
+for filename in *.json; do
+    sed "s/\"folderId\": 6/\"folderId\": $ID/g" $filename > ../list/$filename
+done
+
+ssh $SSH_USER@$SERVER "/usr/bin/kubectl wait --for=condition=ready pod -n it-grafana ${POD} > /dev/null 2>&1"
+
+cd ../list
+for filename in *.json; do
+    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/dashboards/db -d @"$filename" > /dev/null 2>&1
+done
