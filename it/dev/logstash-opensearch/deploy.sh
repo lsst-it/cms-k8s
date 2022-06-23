7167061ee2beb85a4af2d2b002ea2405474116e7
Heinrich Reinking <hreinkinge@gmail.com>
2022-06-23 12:06:23 -0400

    opensearch, logstash and dashboard

---
+++ it/dev/logstash-opensearch/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+kubectl create ns opensearch
+kubectl apply -n opensearch -f opensearch/pdb.yaml
+kubectl apply -n opensearch -f opensearch/secret.yaml
+kubectl apply -n opensearch -f opensearch/configmap.yaml
+kubectl apply -n opensearch -f opensearch/svc-master.yaml
+kubectl apply -n opensearch -f opensearch/svc-headless.yaml
+kubectl apply -n opensearch -f opensearch/statefulset.yaml
+kubectl apply -n opensearch -f dashboard/svc-account.yaml
+kubectl apply -n opensearch -f dashboard/rolebinding.yaml
+kubectl apply -n opensearch -f dashboard/svc.yaml
+kubectl apply -n opensearch -f dashboard/deployment.yaml
+kubectl apply -n opensearch -f logstash/pdb.yaml
+kubectl apply -n opensearch -f logstash/configmap.yaml
+kubectl apply -n opensearch -f logstash/svc-headless.yaml
+kubectl apply -n opensearch -f logstash/svc.yaml
+kubectl apply -n opensearch -f logstash/statefulset.yaml
+kubectl apply -n opensearch -f logstash/ingress.yaml
