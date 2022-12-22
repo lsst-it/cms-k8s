7e46e35a8435db8803e71a8ff3dcccf71f5170b6
Heinrich Reinking <hreinkinge@gmail.com>
2022-12-22 15:23:27 -0300

    update CP site

--- it/cp/prometheus/yagan/deploy.sh
+++ it/cp/prometheus/yagan/deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -xe
+USER=hreinking_b
 
 kubectl create ns yagan-prom
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
+ssh ${USER}@yagan01.cp.lsst.org 'sudo cat /etc/kubernetes/ssl/kube-ca.pem' > ca.crt
+ssh ${USER}@yagan01.cp.lsst.org 'sudo cat /etc/kubernetes/ssl/kube-ca-key.pem' > ca.key
+# Recommend using cfssl https://github.com/cloudflare/cfssl
+cfssl gencert -ca ca.crt -ca-key ca.key etcd-client.json | cfssljson -bare etcd-client
+kubectl appy -f etcd-certs.yaml
 helm install yagan-prom -n yagan-prom prometheus-community/kube-prometheus-stack -f values.yaml
--- it/cp/prometheus/yepun/deploy.sh
+++ it/cp/prometheus/yepun/deploy.sh
@@ -4,4 +4,9 @@ set -xe
 kubectl create ns yepun-prom
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
+ssh ${USER}@yepun01.cp.lsst.org 'sudo cat /etc/kubernetes/ssl/kube-ca.pem' > ca.crt
+ssh ${USER}@yepun01.cp.lsst.org 'sudo cat /etc/kubernetes/ssl/kube-ca-key.pem' > ca.key
+# Recommend using cfssl https://github.com/cloudflare/cfssl
+cfssl gencert -ca ca.crt -ca-key ca.key etcd-client.json | cfssljson -bare etcd-client
+kubectl appy -f etcd-certs.yaml
 helm install yepun-prom -n yepun-prom prometheus-community/kube-prometheus-stack -f values.yaml
