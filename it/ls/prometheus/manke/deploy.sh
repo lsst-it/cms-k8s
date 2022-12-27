370d28b8679b489b899834c7e1c80fd95711865a
Heinrich Reinking <hreinkinge@gmail.com>
2022-12-27 10:00:49 -0300

    add manke

---
+++ it/ls/prometheus/manke/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -xe
+
+kubectl create ns manke-prom
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+ssh ${USER}@manke01.ls.lsst.org 'sudo cat /etc/kubernetes/ssl/kube-ca.pem' > ca.crt
+ssh ${USER}@manke01.ls.lsst.org 'sudo cat /etc/kubernetes/ssl/kube-ca-key.pem' > ca.key
+# Recommend using cfssl https://github.com/cloudflare/cfssl
+cfssl gencert -ca ca.crt -ca-key ca.key etcd-client.json | cfssljson -bare etcd-client
+mv etcd-client.pem etcd-client.crt;mv etcd-client-key.pem etcd-client.key
+kubectl create secret generic etcd-certs -n manke-prom --from-file=ca.crt=ca.crt --from-file=client.crt=etcd-client.crt --from-file=client.key=etcd-client.key --dry-run=client -o yaml > etcd-certs.yaml
+kubectl appy -f etcd-certs.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.61.1/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
+helm install manke-prom -n manke-prom prometheus-community/kube-prometheus-stack -f values.yaml --version=43.1.3
